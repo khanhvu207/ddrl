1831b3ceefd972157e641390484642a7b01292be
khanhvu207 <ngkhanhvu207@gmail.com>
2021-06-05 17:32:46 +0200

    init

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import gym
-env = gym.make('BipedalWalker-v3')
+env = gym.make('BipedalWalkerHardcore-v3')
 env.reset()
 
-for _ in range(100000):
+for t in range(100000):
     env.render()
-    env.step(env.action_space.sample()) # take a random action
+    action = env.action_space.sample() # take a random action
+    observation, reward, done, info = env.step(action)
+    if done:
+        print("Episode finished after {} timesteps".format(t+1))
+        break
+
 env.close()

f71cfad1e6b4e203f5f10589a7e8f2200deae128
khanhvu207 <ngkhanhvu207@gmail.com>
2021-06-11 13:48:09 +0200

    add: exploration noise

---
+++ ddrl/utils.py
@@ -0,0 +1,32 @@
+import copy
+import random
+import numpy as np
+
+class OUNoise:
+    """
+        Ornstein-Uhlenbeck process.
+        Reference: https://github.com/xkiwilabs/Multi-Agent-DDPG-using-PTtorch-and-ML-Agents/blob/master/OUNoise.py
+    """
+    def __init__(self, size, seed, mu=0.0, theta=0.1, sigma=.2, sigma_min=.1, sigma_decay=.99):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.sigma_min = sigma_min
+        self.sigma_decay = sigma_decay
+        self.seed = random.seed(seed)
+        self.size = size
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+        """Resduce  sigma from initial value to min"""
+        self.sigma = max(self.sigma_min, self.sigma*self.sigma_decay)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
+        self.state = x + dx
+        return self.state

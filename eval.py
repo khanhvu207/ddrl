6355ced04648774023a1f842e0e765bc79bacd9f
khanhvu207 <ngkhanhvu207@gmail.com>
2021-06-30 15:24:18 +0200

    adapt fire

--- eval.py
+++ eval.py
@@ -1,20 +1,25 @@
+import fire
+import yaml
 import gym
 import torch
 from ddrl.agent import Agent
 
 env_name = 'LunarLanderContinuous-v2'
 
-if __name__ == '__main__':
-    env = gym.make(env_name)
+def main(config=None):
+    config = yaml.load(open(config, "r"), Loader=yaml.Loader)
+    env = gym.make(config['env']['env-name'])
     obs_dim = env.observation_space.shape[0]
     act_dim = env.action_space.shape[0]
-    agent = Agent(state_size=obs_dim, action_size=act_dim)
+    agent = Agent(state_size=obs_dim, action_size=act_dim, config=config)
 
-    agent.Actor.load_state_dict(torch.load('actor_weight.pth'))
-    agent.Critic.load_state_dict(torch.load('critic_weight.pth'))
+    agent.sync(actor_weight=torch.load('actor_weight.pth'), critic_weight=torch.load('critic_weight.pth'))
 
     observation = env.reset()
     while True:
         env.render()
         action, log_prob, value = agent.act(observation, is_train=False)
         observation, reward, done, info = env.step(action)
+
+if __name__ == '__main__':
+    fire.Fire(main)

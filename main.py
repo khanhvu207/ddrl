cc3b08c0ec15fc9910c66cae7420485e535f4ea6
khanhvu207 <ngkhanhvu207@gmail.com>
2021-07-10 14:13:18 +0200

    debug flag

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from re import sub
 import fire
 import subprocess, signal, time
 
-def main(wallclock_time=None):
+def main(wallclock_time=None, debug=False):
     if wallclock_time is None:
         raise Exception()
 
     print("Starting...")
+    debug_mode = 'True' if debug else 'False'
     learner = subprocess.Popen(['python3', 'run_learner.py', '--config=configs//OpenAI//LunarLanderContinuous.yaml'])
-    worker = subprocess.Popen(['python3', 'run_worker.py', '--config=configs//OpenAI//LunarLanderContinuous.yaml', '--debug=True'])
+    worker = subprocess.Popen(['python3', 'run_worker.py', '--config=configs//OpenAI//LunarLanderContinuous.yaml', f'--debug={debug_mode}'])
 
     time.sleep(60 * wallclock_time) # <- 60 seconds x minutes
 
